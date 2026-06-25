571c6bf6c143dbb825e881c228ba038f640e178b
Peifa <ciekawe320@gmail.com>
2026-06-25 14:07:30 +0200

    fix: remove .vercel/output from repo, add to gitignore

--- .vercel/output/functions/__server.func/_tanstack-start-manifest_v-DAkT5feV.mjs
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ routes: { __root__: { filePath: "C:/Users/Simon/Downloads/mai-rewards-clone/src/routes/__root.tsx", children: ["/"], preloads: ["/assets/index-dVjlIdeZ.js"], scripts: [{ attrs: { type: "module", async: true, src: "/assets/index-dVjlIdeZ.js" } }] }, "/": { filePath: "C:/Users/Simon/Downloads/mai-rewards-clone/src/routes/index.tsx", children: void 0, preloads: ["/assets/index-Cu4SM-_U.js"] } } });
-export {
-  tsrStartManifest
-};
